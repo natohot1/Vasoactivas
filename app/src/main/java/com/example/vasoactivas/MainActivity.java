5a5b975581ff81ed5326b39159258c09a22d7695
natohot1 <f_havarez@hotmail.com>
2021-09-11 11:38:44 +0200

    Final dosis fenitoina

--- app/src/main/java/com/example/vasoactivas/MainActivity.java
+++ app/src/main/java/com/example/vasoactivas/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     NumberPicker medicamentoPicker, pickerSolvente, pickerPeso;
     String[] array_soloNombresMedicamentos, array_dopaInicio, array_noradrenalina,array_solventes,array_pesos;
     String medBuscado = "DOBUTAMINA",comercialBuscado ="DOBUTAMINA HOSPIRA";
-    int dosisNumero,peso,cantidad,cantidadMg,contador, contador2;
+    int dosisNumero,peso, cantidadSuero,cantidadMg,contador, contador2;
     DecimalFormat form = new DecimalFormat("0.0");
     DBHelper manager;
     String arrayMedicamento[] = new String[5];
@@ -38,14 +38,14 @@ public class MainActivity extends AppCompatActivity {
 
         array_Dosis  =manager.dosis(medBuscado);
        inicilizar();
-       cantidad = 200;
+       cantidadSuero = 200;
        cantidadMg = 200;
 
        peso = 60;
 
         txAtributos.setText(""+ arrayMedicamento[1]+" "+ arrayMedicamento[2]+" mg en "+ arrayMedicamento[3]+" ml");
         cambiodosis();
-        calcularSegun(array_Dosis);
+        calcularSegun();
     }
 
     private void inicilizar() {
@@ -67,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
         array_noradrenalina = getResources().getStringArray(R.array.arraynoradrenalina);
         medicamentoPicker = findViewById(R.id.numberPicker);
         medicamentoPicker.setMinValue(0);
-        medicamentoPicker.setMaxValue(array_soloNombresMedicamentos.length-1);
-        medicamentoPicker.setDisplayedValues(array_soloNombresMedicamentos);
+        medicamentoPicker.setMaxValue(arrayNombresComerciales.length-1);
+        medicamentoPicker.setDisplayedValues(arrayNombresComerciales);
         pickerPeso = findViewById(R.id.pickerPeso);
         pickerPeso.setMinValue(10);
         pickerPeso.setMaxValue(200);
@@ -87,35 +87,39 @@ public class MainActivity extends AppCompatActivity {
                 comercialBuscado = arrayNombresComerciales[nuevoVal];
                 arrayMedicamento = manager.medicinaComerial(comercialBuscado);
                 array_Dosis = manager.dosis(medBuscado);
+                actualizar();
                 texDrogaTitulo.setText(arrayNombresComerciales[nuevoVal]);
-                dosisNumero = nuevoVal;
-
-              if (dosisNumero == 2){
+              if (medBuscado.equals("DOPAMINA")){
                   txtDosis.setText(String.format("2 ug/kg/min"));
                   txtDosis2.setText(String.format("5 ug/kg/min"));
                   txtDosis3.setText(String.format("7 ug/kg/min"));
                 //  medBuscado = arrayMedicamento[0];
               }
-              if (dosisNumero == 1){
+                if (medBuscado.equals("DOBUTAMINA")){
                   txtDosis.setText(String.format("2 ug/kg/min"));
                   txtDosis2.setText(String.format("20 ug/kg/min"));
                   txtDosis3.setText(String.format(""));
                  // medBuscado = arrayMedicamento[0];
               }
-              if (dosisNumero == 4){
+                if (medBuscado.equals("NORADRENALINA")){
                   txtDosis.setText(String.format("%s", array_noradrenalina[0]));
                   txtDosis2.setText(String.format("%s", array_noradrenalina[1]));
                   txtDosis3.setText(String.format(""));
                  // medBuscado = arrayMedicamento[0];
               }
-              if (dosisNumero == 3){
-                  txtDosis.setText(String.format("30 ug/min"));
+                if (medBuscado.equals("NITROGLICERINA")){
+                  txtDosis.setText(String.format("ug/min"));
                   txtDosis2.setText(String.format(""));
                   txtDosis3.setText(String.format(""));
                //   medBuscado = arrayMedicamento[0];
               }
+                if (medBuscado.equals("FENITOINA")){
+                  txtDosis.setText(String.format("17 mg/kg"));
+                  txtDosis2.setText(String.format("6 mg/kg/24 h"));
+                  txtDosis3.setText(String.format(""));
+              }
                 cambiodosis();
-                calcularSegun(array_Dosis);
+                calcularSegun();
            //  calcular(peso);
             }
         });
@@ -127,15 +131,15 @@ public class MainActivity extends AppCompatActivity {
         pickerSolvente.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker numberPicker, int antVal, int nuevoVal) {
-                if (nuevoVal ==0){ cantidad = 50;}
-                if (nuevoVal ==1){ cantidad = 100; }
-                if (nuevoVal ==2){ cantidad = 200;}
-                if (nuevoVal ==3){ cantidad = 250; }
-                if (nuevoVal ==4){ cantidad = 500;}
-                if (nuevoVal ==5){ cantidad = 1000; }
+                if (nuevoVal ==0){ cantidadSuero = 50;}
+                if (nuevoVal ==1){ cantidadSuero = 100; }
+                if (nuevoVal ==2){ cantidadSuero = 200;}
+                if (nuevoVal ==3){ cantidadSuero = 250; }
+                if (nuevoVal ==4){ cantidadSuero = 500;}
+                if (nuevoVal ==5){ cantidadSuero = 1000; }
                 cambiodosis();
                 //calcular(peso);
-                calcularSegun(array_Dosis);
+                calcularSegun();
             }
         });
         pickerPeso.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
@@ -144,13 +148,18 @@ public class MainActivity extends AppCompatActivity {
                 peso = newVal;
                 cambiodosis();
                // calcular(peso);
-                calcularSegun(array_Dosis);
+                calcularSegun();
             }
         });
         }
 
-        //CALCULAR SEGUN MEDICAMENTO
-    private void calcularSegun(String []array_DosisPasado){
+    private void actualizar() {
+
+    }
+
+    //CALCULAR SEGUN MEDICAMENTO
+    private void calcularSegun(){
+        String fenitoina24="";
         cantidadMg=Integer.valueOf(arrayMedicamento[2]);
         txtml.setText(null);
         txtml2.setText(null);
@@ -162,19 +171,19 @@ public class MainActivity extends AppCompatActivity {
             if((array_Dosis[3]).equals("ug/min")){
                 //DOSIS NITROGLICERINA EMPEZAR A ESTA DOSIS Y SE PUEDE AUMENTYAR HASTA 10 VECES
                 double dosisPorKilo = Double.valueOf(array_Dosis[2]);
-                priDosis = (dosisPorKilo*cantidad)/ampollaMicroGramos;
+                priDosis = (dosisPorKilo* cantidadSuero)/ampollaMicroGramos;
             }
             if((array_Dosis[3]).equals("ug/kg/min")){
-                double dosis = Double.valueOf(array_DosisPasado[2]);
-                priDosis = ((dosis*peso)*60/(ampollaMicroGramos/cantidad));
+                double dosis = Double.valueOf(array_Dosis[2]);
+                priDosis = ((dosis*peso)*60/(ampollaMicroGramos/ cantidadSuero));
                 if(array_Dosis[5]!=null) {
                     if ((array_Dosis[5]).equals("ug/kg/min")) {
-                        double dosis1 = Double.valueOf(array_DosisPasado[4]);
-                        segDosis = ((dosis1 * peso) * 60 / (ampollaMicroGramos / cantidad));
+                        double dosis1 = Double.valueOf(array_Dosis[4]);
+                        segDosis = ((dosis1 * peso) * 60 / (ampollaMicroGramos / cantidadSuero));
                         if(array_Dosis[7]!=null) {
                             if ((array_Dosis[7]).equals("ug/kg/min")) {
-                                double dosis2 = Double.valueOf(array_DosisPasado[6]);
-                                terDosis = ((dosis2 * peso) * 60 / (ampollaMicroGramos / cantidad));
+                                double dosis2 = Double.valueOf(array_Dosis[6]);
+                                terDosis = ((dosis2 * peso) * 60 / (ampollaMicroGramos / cantidadSuero));
                             }
                         }
                     }
@@ -184,26 +193,26 @@ public class MainActivity extends AppCompatActivity {
                 double ampollaMg =Double.valueOf(arrayMedicamento[2]); //100 oh 250 mg de fentoina
                 double ampollaCC =Double.valueOf(arrayMedicamento[3]); //2 oh 5 cc de fentoina
                 double dosis8 = Double.valueOf(array_Dosis[2]);
-                double dosis9 = Double.valueOf(array_Dosis[4]); //dosis de 24 horas
+                double dosis9 = Double.valueOf(array_Dosis[4]); //dosis de 24 horas PARA  FENITOINA
                 if(medBuscado.equals("FENITOINA")){
                     //MG PO KILO DOSIS DE ATAQUE EN 20 MINUTOS
                     double mgKg = (peso*dosis8);
                     //cantidad de ampollas para el peso 2 o 5 cc
-                    priDosis = (mgKg/ampollaMg)*ampollaCC; //dosisAmpollas
+                    priDosis = mgKg/ampollaMg; //dosisAmpollas
                     //MG PO KILO DOSIS DE MANTENIMIENTO EN 24 HORAS
                     double mgKg24 = (peso*dosis9);
+                    mgKg24 = mgKg24/ampollaMg; // NUMERO DE AMPOLLAS
                     //cantidad de ampollas para el peso 2 o 5 cc
-                    segDosis = (mgKg24/ampollaMg)*ampollaCC; //dosisAmpollas
-                  //  txtml.setText(priDosis+" cc, en "+cantidad+" cc de Salino, en 20 minutos");
-                  //  txtml2.setText(segDosis+" cc, en "+cantidad+" cc de Salino en 24 horas");
+                    segDosis = cantidadSuero/24; //PASAR CC HORA EN 24 HORAS
+                    fenitoina24=(form.format(mgKg24)+" ampollas en "+form.format(cantidadSuero)+" cc S. salina, a "+form.format(segDosis)+" ml/h");
                 }
             }
 
 
 
-        if(medBuscado.equals("FENITOINA")){
-            txtml.setText(priDosis+" cc, en "+cantidad+" cc de Salino, en 20 minutos");
-            txtml2.setText(segDosis+" cc, en "+cantidad+" cc de Salino en 24 horas");
+        if(medBuscado.contains("FENITOINA")){
+            txtml.setText(form.format(priDosis)+" ampollas, en "+ cantidadSuero +" cc de Salino, en 20 minutos");
+            txtml2.setText(fenitoina24);
 
         }else {
             if(priDosis >0) {
@@ -225,21 +234,21 @@ public class MainActivity extends AppCompatActivity {
         double segDosis = 0;
         double terDosis = 0;
         if (dosisNumero ==0){
-            priDosis = ((2.5*peso)*60/(200000/cantidad));
-            segDosis = ((5*peso)*60/(200000/cantidad));
-            terDosis = ((7*peso)*60/(200000/cantidad));
+            priDosis = ((2.5*peso)*60/(200000/ cantidadSuero));
+            segDosis = ((5*peso)*60/(200000/ cantidadSuero));
+            terDosis = ((7*peso)*60/(200000/ cantidadSuero));
 
         }
         if (dosisNumero ==1){
-            priDosis = (((2*peso)*60)*cantidad)/(250000);
-            segDosis = (((20*peso)*60)*cantidad)/(250000);
+            priDosis = (((2*peso)*60)* cantidadSuero)/(250000);
+            segDosis = (((20*peso)*60)* cantidadSuero)/(250000);
         }
         if (dosisNumero ==2){
-            priDosis = ((0.05*peso)*60/(200000/cantidad));
-            segDosis = ((0.5*peso)*60/(200000/cantidad));
+            priDosis = ((0.05*peso)*60/(200000/ cantidadSuero));
+            segDosis = ((0.5*peso)*60/(200000/ cantidadSuero));
         }
         if (dosisNumero ==3){
-            segDosis = ((30*peso)*60/(200000/cantidad));
+            segDosis = ((30*peso)*60/(200000/ cantidadSuero));
         }
         txtml.setText("");
         txtml2.setText("");
